17cd7da09513d69c0df155636506b33e10c2ae7f
Christopher Bishop <cbishop@solutionstream.com>
2023-05-03 16:58:43 -0600

    pass form to items

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,21 +1,22 @@
-import './NewExpense.css'
-import ExpenseForm from './ExpenseForm.js';
+import React from 'react';
+
+import ExpenseForm from './ExpenseForm';
+import './NewExpense.css';
 
 const NewExpense = (props) => {
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
       id: Math.random().toString()
-    }
-
-    props.onAddExpenseData(expenseData);
-  }
+    };
+    props.onAddExpense(expenseData);
+  };
 
   return (
-    <div className="new-expense">
-      <ExpenseForm onSaveExpense={saveExpenseDataHandler} />
+    <div className='new-expense'>
+      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
     </div>
-  )
-}
+  );
+};
 
 export default NewExpense;
