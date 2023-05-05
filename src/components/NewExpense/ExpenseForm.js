e4e6f060bba634d0e74431f25e219749930dcfee
Christopher Bishop <cbishop@solutionstream.com>
2023-05-05 14:44:15 -0600

    Adds cancel functionality

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -3,6 +3,8 @@ import React, { useState } from 'react';
 import './ExpenseForm.css';
 
 const ExpenseForm = (props) => {
+  
+
   const [enteredTitle, setEnteredTitle] = useState('');
   const [enteredAmount, setEnteredAmount] = useState('');
   const [enteredDate, setEnteredDate] = useState('');
@@ -87,6 +89,7 @@ const ExpenseForm = (props) => {
         </div>
       </div>
       <div className='new-expense__actions'>
+        <button onClick={props.onCancel}>Cancel</button>
         <button type='submit'>Add Expense</button>
       </div>
     </form>
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,20 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import ExpenseForm from './ExpenseForm';
 import './NewExpense.css';
 
 const NewExpense = (props) => {
+  const [showForm, setShowForm] = useState(false);
+
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
       id: Math.random().toString()
     };
     props.onAddExpense(expenseData);
+    setShowForm(false);
   };
+ 
+
+  const addNewExpenseHandler = () => {
+    setShowForm(true);
+  }
+
+  const cancelNewExpense = () => {
+    setShowForm(false);
+  }
 
   return (
     <div className='new-expense'>
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+      { !showForm && <button onClick={addNewExpenseHandler}>Add New Expense</button>}
+      { showForm && <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} onCancel={cancelNewExpense} />}
     </div>
   );
 };
